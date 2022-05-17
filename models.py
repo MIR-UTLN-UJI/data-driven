3f4dfbc4b24a51c0aaf1a83122d657c850bcd32a
ahmedharbii <ahmedharbii10@gmail.com>
2022-05-17 02:13:40 +0200

    Add architecture for RNN

--- models.py
+++ models.py
@@ -1,7 +1,27 @@
 import torch
 
 
-#class MobileRNN(torch.nn.Module):
+class MobileRNN(torch.nn.Module):
+    def __init__(self, weights, output_size, hidden_size, num_layers=2):
+        super().__init__()
+        self.mn = load_model(weights, output_size=output_size)
+        self.rnn = torch.nn.RNN(input_size=output_size, \
+            hidden_size=hidden_size, num_layers=num_layers)
+        self.linear = torch.nn.Linear(hidden_size, 7)
+    
+    def forward(self, x):
+        images = [x[:, 3*i:3*(i+1), :, :] for i in range(x.shape[1]/3)]
+        mn_images = [self.mn(im).unsqueeze(0) for im in images]
+        # first_image = x[:, :3, :, :] #batch_size, num_channels (first 3), width, height
+        # second_image = x[:, 3:, :, :] #batch_size, num_channels (last 3), width, height
+        # mn_first_image = self.mn(first_image).unsqueeze(0)
+        # mn_second_image = self.mn(second_image).unsqueeze(0)
+        # mn_image_first_second = torch.cat((mn_first_image,mn_second_image), dim=0)
+        mn_images_concatenated= torch.cat(mn_images, dim=0)
+        out, hn = self.rnn(mn_images_concatenated)
+        pose = self.linear(out[-1]) # take output of last time step
+        return pose
+
 
 
 
